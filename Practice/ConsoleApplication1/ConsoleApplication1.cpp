1a060c3d0a411b337b5b9810d58441b826f00a0e
cijik <cijik1@gmail.com>
2016-06-19 21:12:33 +0300

    2

--- Practice/ConsoleApplication1/ConsoleApplication1.cpp
+++ Practice/ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,15 +15,15 @@ struct stud {
 
 int main(void) {
 	stud Student[10] = { { "Sergey", "Ivanov", "Chisinau", 7.0, "069785649" },
-	{ "Vladimir", "Novicov", "Balti", 6.3, "079885713" },
-	{ "Maria", "Frolova", "Chisinau", 9.1, "068457835" },
-	{ "Vladislav", "Petuhov", "Chisinau", 7.2, "068992334" },
-	{ "Evgheniy", "Grishin", "Cahul", 6.6, "079876745" },
-	{ "Mihail", "Ostrovskiy", "Chisinau", 7.8, "076905090" },
-	{ "Svetlana", "Surkova", "Chisinau", 8.0, "068679492" },
-	{ "Vasiliy", "Terem", "Ungheni", 6.9, "060564600" },
-	{ "Valentin", "Voskoboinikov", "Edinet", 6.0, "078671084" },
-	{ "Grigoriy", "Andreev", "Chisinau", 7.6, "079488834" } };
+						{ "Vladimir", "Novicov", "Balti", 6.3, "079885713" },
+						{ "Maria", "Frolova", "Chisinau", 9.1, "068457835" },
+						{ "Vladislav", "Petuhov", "Chisinau", 7.2, "068992334" },
+						{ "Evgheniy", "Grishin", "Cahul", 6.6, "079876745" },
+						{ "Mihail", "Ostrovskiy", "Chisinau", 7.8, "076905090" },
+						{ "Svetlana", "Surkova", "Chisinau", 8.0, "068679492" },
+						{ "Vasiliy", "Terem", "Ungheni", 6.9, "060564600" },
+						{ "Valentin", "Voskoboinikov", "Edinet", 6.0, "078671084" },
+						{ "Grigoriy", "Andreev", "Chisinau", 7.6, "079488834" } };
 
 	cout << endl << "Students from Chisinau:" << endl;
 	for (int i = 0; i<10; i++)
@@ -31,11 +31,11 @@ int main(void) {
 			cout << Student[i].firstName << " " << Student[i].lastName << endl;
 	cout << endl << "Students from Chisinau with points over 7:" << endl;
 	for (int i = 0; i<10; i++)
-		if (Student[i].city == "Chisinau" && Student[i].points>7)
+		if (Student[i].city == "Chisinau" && Student[i].points > 7)
 			cout << Student[i].firstName << " " << Student[i].lastName << endl;
 	cout << endl << "Students with points between 5 and 8" << endl;
 	for (int i = 0; i<10; i++)
-		if (Student[i].points>5 && Student[i].points<8)
+		if (Student[i].points>5 && Student[i].points < 8)
 			cout << Student[i].firstName << " " << Student[i].lastName << endl;
 	cout << endl << "Students with Orange mobile provider" << endl;
 	for (int i = 0; i<10; i++)
